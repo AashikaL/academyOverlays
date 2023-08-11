c0fab05681689bf5f839b467aa7ae195a045d92e
AashikaL <aashikaaashika1799@gmail.com>
2023-08-11 11:43:20 +0530

    update

--- src/app/my-modal/my-modal.page.ts
+++ src/app/my-modal/my-modal.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-my-modal',
@@ -7,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyModalPage implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {
   }
   dismiss(){
-    
+    this.modalCtrl.dismiss()
   }
 
 }
